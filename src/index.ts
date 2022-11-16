ad61e2b9ff090d4625b92e312d37e1b5df23cfb4
mckeen <mcasmakeen@gmail.com>
2022-11-16 16:33:12 +0800

    minor fix

--- src/index.ts
+++ src/index.ts
@@ -10,26 +10,26 @@ import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
-  const orm = await MikroORM.init(microConfig);
-  await orm.getMigrator().up();
+	const orm = await MikroORM.init(microConfig);
+	await orm.getMigrator().up();
 
-  const app = express();
+	const app = express();
 
-  const apolloServer = new ApolloServer({
-    schema: await buildSchema({
-      resolvers: [HelloResolver, PostResolver],
-      validate: false,
-    }),
-    context: () => ({ em: orm.em }),
-  });
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [HelloResolver, PostResolver],
+			validate: false,
+		}),
+		context: () => ({ em: orm.em }),
+	});
 
-  apolloServer.applyMiddleware({ app });
+	apolloServer.applyMiddleware({ app });
 
-  app.listen(4000, () => {
-    console.log("server started on localhost:4000");
-  });
+	app.listen(3000, () => {
+		console.log("server started on localhost:4000");
+	});
 };
 
 main().catch((err) => {
-  console.error(err);
+	console.error(err);
 });
