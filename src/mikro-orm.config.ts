fd2190e2e747f3ef5680acea16c3157627a16824
mckeen <mcasmakeen@gmail.com>
2022-11-16 11:29:01 +0800

    db config

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -4,12 +4,14 @@ import { MikroORM } from "@mikro-orm/core";
 import path from "path";
 
 export default {
-  migrations: {
-    path: path.join(__dirname, "./migrations"),
-    pattern: /^[\w-]+\d+\.[tj]s$/,
-  },
-  entities: [Post],
-  dbName: "lireddit",
-  type: "postgresql",
-  debug: !__prod__,
+	migrations: {
+		path: path.join(__dirname, "./migrations"),
+		pattern: /^[\w-]+\d+\.[tj]s$/,
+	},
+	entities: [Post],
+	dbName: "lireddit",
+	type: "postgresql",
+	user: "postgres",
+	password: "postgres",
+	debug: !__prod__,
 } as Parameters<typeof MikroORM.init>[0];
